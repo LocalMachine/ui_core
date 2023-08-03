5c60c5f8f842b66d5b5971aebc41050dbb95a5fe
Manukyan Rachik <rmanukyan@softailor.ru>
2023-08-03 14:48:13 +0300

    create empty project

---
+++ src/main/java/com/test/ru/ui_core/UiCoreApplication.java
@@ -0,0 +1,13 @@
+package com.test.ru.ui_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiCoreApplication.class, args);
+	}
+
+}
